3c0ad0d5b01e71938e14726f46f8270ad5914e08
lrosa <lrosa@DT07017.mercador.local>
2016-06-30 13:28:31 -0300

    Adicionada criação do DAO por injeção de dependências.

--- src/br/com/caelum/contas/controller/ContaController.java
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,52 +18,57 @@ import br.com.caelum.contas.modelo.Conta;
 @Controller
 public class ContaController {
 
-	private final ContaDAO dao = new ContaDAO();
+    private final ContaDAO dao;
 
-	@RequestMapping
-	public String form() {
-		return "conta/formulario";
-	}
+    @Autowired
+    public ContaController(final ContaDAO dao) {
+        this.dao = dao;
+    }
 
-	@RequestMapping("/adicionaConta")
-	public String adiciona(@Valid final Conta conta, final BindingResult result) {
-		if(result.hasErrors()) {
-			return "conta/formulario";
-		}
-		dao.adiciona(conta);
-		return "conta/conta-adicionada";
-	}
+    @RequestMapping
+    public String form() {
+        return "conta/formulario";
+    }
 
-	@RequestMapping("/listaContas")
-	public ModelAndView listaContas() {
-		final List<Conta> contas = dao.lista();
-		final ModelAndView modelAndView = new ModelAndView("conta/listaContas");
-		modelAndView.addObject("contas", contas);
-		return modelAndView;
-	}
+    @RequestMapping("/adicionaConta")
+    public String adiciona(@Valid final Conta conta, final BindingResult result) {
+        if(result.hasErrors()) {
+            return "conta/formulario";
+        }
+        dao.adiciona(conta);
+        return "conta/conta-adicionada";
+    }
 
-	@RequestMapping("/removeConta")
-	public String remove(final Conta conta) {
-		dao.remove(conta);
-		return "redirect:listaContas";
-	}
+    @RequestMapping("/listaContas")
+    public ModelAndView listaContas() {
+        final List<Conta> contas = dao.lista();
+        final ModelAndView modelAndView = new ModelAndView("conta/listaContas");
+        modelAndView.addObject("contas", contas);
+        return modelAndView;
+    }
 
-	@RequestMapping("/mostraConta")
-	public String mostra(final Long id, final Model model) {
-		model.addAttribute("conta", dao.buscaPorId(id));
-		return "conta/mostra";
-	}
+    @RequestMapping("/removeConta")
+    public String remove(final Conta conta) {
+        dao.remove(conta);
+        return "redirect:listaContas";
+    }
 
-	@RequestMapping("/alteraConta")
-	public String altera(final Conta conta) {
-		dao.altera(conta);
-		return "redirect:listaContas";
-	}
+    @RequestMapping("/mostraConta")
+    public String mostra(final Long id, final Model model) {
+        model.addAttribute("conta", dao.buscaPorId(id));
+        return "conta/mostra";
+    }
 
-	@RequestMapping("/pagaConta")
-	public void paga(final Long id, final HttpServletResponse response) {
-		dao.paga(id);
-		response.setStatus(200);
-	}
+    @RequestMapping("/alteraConta")
+    public String altera(final Conta conta) {
+        dao.altera(conta);
+        return "redirect:listaContas";
+    }
+
+    @RequestMapping("/pagaConta")
+    public void paga(final Long id, final HttpServletResponse response) {
+        dao.paga(id);
+        response.setStatus(200);
+    }
 
 }
--- src/br/com/caelum/contas/dao/ContaDAO.java
+++ src/br/com/caelum/contas/dao/ContaDAO.java
@@ -9,159 +9,166 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import br.com.caelum.contas.ConnectionFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import br.com.caelum.contas.modelo.Conta;
 import br.com.caelum.contas.modelo.TipoDaConta;
-
+@Repository
 public class ContaDAO {
-	private Connection connection;
 
-	public ContaDAO() {
-		try {
-			this.connection = new ConnectionFactory().getConnection();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
 
-	public void adiciona(Conta conta) {
-		String sql = "insert into contas (descricao, paga, valor, tipo) values (?,?,?,?)";
-		PreparedStatement stmt;
-		try {
-			stmt = connection.prepareStatement(sql);
-			stmt.setString(1, conta.getDescricao());
-			stmt.setBoolean(2, conta.isPaga());
-			stmt.setDouble(3, conta.getValor());
-			stmt.setString(4, conta.getTipo().name());
-			stmt.execute();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-		
-	}
+    private final Connection connection;
 
-	public void remove(Conta conta) {
+    @Autowired
+    public ContaDAO(final DataSource dataSource) {
+        try {
+            this.connection = dataSource.getConnection();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		if (conta.getId() == null) {
-			throw new IllegalStateException("Id da conta naoo deve ser nula.");
-		}
+    public void adiciona(final Conta conta) {
+        final String sql = "insert into contas (descricao, paga, valor, tipo) values (?,?,?,?)";
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, conta.getDescricao());
+            stmt.setBoolean(2, conta.isPaga());
+            stmt.setDouble(3, conta.getValor());
+            stmt.setString(4, conta.getTipo().name());
+            stmt.execute();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-		String sql = "delete from contas where id = ?";
-		PreparedStatement stmt;
-		try {
-			stmt = connection.prepareStatement(sql);
-			stmt.setLong(1, conta.getId());
-			stmt.execute();
-			
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    }
 
-	public void altera(Conta conta) {
-		String sql = "update contas set descricao = ?, paga = ?, dataPagamento = ?, tipo = ?, valor = ? where id = ?";
-		PreparedStatement stmt;
-		try {
-			stmt = connection.prepareStatement(sql);
-			stmt.setString(1, conta.getDescricao());
-			stmt.setBoolean(2, conta.isPaga());
-			stmt.setDate(3, conta.getDataPagamento() != null ? new Date(conta
-					.getDataPagamento().getTimeInMillis()) : null);
-			stmt.setString(4, conta.getTipo().name());
-			stmt.setDouble(5, conta.getValor());
-			stmt.setLong(6, conta.getId());
-			stmt.execute();
-			
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public void remove(final Conta conta) {
 
-	public List<Conta> lista() {
-		try {
-			List<Conta> contas = new ArrayList<Conta>();
-			PreparedStatement stmt = this.connection
-					.prepareStatement("select * from contas");
+        if (conta.getId() == null) {
+            throw new IllegalStateException("Id da conta naoo deve ser nula.");
+        }
 
-			ResultSet rs = stmt.executeQuery();
+        final String sql = "delete from contas where id = ?";
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setLong(1, conta.getId());
+            stmt.execute();
 
-			while (rs.next()) {
-				// adiciona a conta na lista
-				contas.add(populaConta(rs));
-			}
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-			rs.close();
-			stmt.close();
+    public void altera(final Conta conta) {
+        final String sql = "update contas set descricao = ?, paga = ?, dataPagamento = ?, tipo = ?, valor = ? where id = ?";
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, conta.getDescricao());
+            stmt.setBoolean(2, conta.isPaga());
+            stmt.setDate(3, conta.getDataPagamento() != null ? new Date(conta
+                .getDataPagamento().getTimeInMillis()) : null);
+            stmt.setString(4, conta.getTipo().name());
+            stmt.setDouble(5, conta.getValor());
+            stmt.setLong(6, conta.getId());
+            stmt.execute();
 
-			return contas;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public Conta buscaPorId(Long id) {
+    public List<Conta> lista() {
+        try {
+            final List<Conta> contas = new ArrayList<Conta>();
+            final PreparedStatement stmt = this.connection
+                .prepareStatement("select * from contas");
 
-		
-		if (id == null) {
-			throw new IllegalStateException("Id da conta nao deve ser nula.");
-		}
+            final ResultSet rs = stmt.executeQuery();
 
-		try {
-			PreparedStatement stmt = this.connection
-					.prepareStatement("select * from contas where id = ?");
-			stmt.setLong(1, id);
-			ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                // adiciona a conta na lista
+                contas.add(populaConta(rs));
+            }
 
-			if (rs.next()) {
-				return populaConta(rs);
-			}
+            rs.close();
+            stmt.close();
 
-			rs.close();
-			stmt.close();
-			
-			return null;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+            return contas;
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public void paga(Long id) {
+    public Conta buscaPorId(final Long id) {
 
-		if (id == null) {
-			throw new IllegalStateException("Id da conta nao deve ser nula.");
-		}
 
-		String sql = "update contas set paga = ?, dataPagamento = ? where id = ?";
-		PreparedStatement stmt;
-		try {
-			stmt = connection.prepareStatement(sql);
-			stmt.setBoolean(1, true);
-			stmt.setDate(2, new Date(Calendar.getInstance().getTimeInMillis()));
-			stmt.setLong(3, id);
-			stmt.execute();
+        if (id == null) {
+            throw new IllegalStateException("Id da conta nao deve ser nula.");
+        }
 
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        try {
+            final PreparedStatement stmt = this.connection
+                .prepareStatement("select * from contas where id = ?");
+            stmt.setLong(1, id);
+            final ResultSet rs = stmt.executeQuery();
 
-	private Conta populaConta(ResultSet rs) throws SQLException {
-		Conta conta = new Conta();
+            if (rs.next()) {
+                return populaConta(rs);
+            }
 
-		conta.setId(rs.getLong("id"));
-		conta.setDescricao(rs.getString("descricao"));
-		conta.setPaga(rs.getBoolean("paga"));
-		conta.setValor(rs.getDouble("valor"));
+            rs.close();
+            stmt.close();
 
-		Date data = rs.getDate("dataPagamento");
-		if (data != null) {
-			Calendar dataPagamento = Calendar.getInstance();
-			dataPagamento.setTime(data);
-			conta.setDataPagamento(dataPagamento);
-		}
-		
-		conta.setTipo(Enum.valueOf(TipoDaConta.class, rs.getString("tipo")));
-		
-		return conta;
-	}
+            return null;
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void paga(final Long id) {
+
+        if (id == null) {
+            throw new IllegalStateException("Id da conta nao deve ser nula.");
+        }
+
+        final String sql = "update contas set paga = ?, dataPagamento = ? where id = ?";
+        PreparedStatement stmt;
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setBoolean(1, true);
+            stmt.setDate(2, new Date(Calendar.getInstance().getTimeInMillis()));
+            stmt.setLong(3, id);
+            stmt.execute();
+
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Conta populaConta(final ResultSet rs) throws SQLException {
+        final Conta conta = new Conta();
+
+        conta.setId(rs.getLong("id"));
+        conta.setDescricao(rs.getString("descricao"));
+        conta.setPaga(rs.getBoolean("paga"));
+        conta.setValor(rs.getDouble("valor"));
+
+        final Date data = rs.getDate("dataPagamento");
+        if (data != null) {
+            final Calendar dataPagamento = Calendar.getInstance();
+            dataPagamento.setTime(data);
+            conta.setDataPagamento(dataPagamento);
+        }
+
+        conta.setTipo(Enum.valueOf(TipoDaConta.class, rs.getString("tipo")));
+
+        return conta;
+    }
 }
